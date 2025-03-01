61a5a1dc777b0b84c31c8408633564cc94c4257c
gustavo <gugamathiaz12@gmail.com>
2025-03-01 08:03:27 -0300

    Add new Router: Login

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home.tsx";
 import Content from "./pages/content.tsx";
+import Login from "./pages/login.tsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login/>} />
                 <Route path="/login/home" element={<Content />} />
             </Routes>
         </BrowserRouter>
