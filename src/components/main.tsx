2aaeb8de099cb56e110fdcb62a12f84ae5fe0129
gustavo andré <gustavo.andrs21@gmail.com>
2025-05-21 17:06:47 -0300

    :package: entering image path from public folder

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,7 +3,7 @@ import Input from "./input";
 function Main() {
     return (
         <main>
-            <img src="./src/assets/career-53.svg" alt="Ilustração com personagem subindo degrais ao topo." />
+            <img src="../../public/career.svg" alt="Ilustração com personagem subindo degrais ao topo." />
 
             <form className='main-content'>
                 <h1>O <span>conhecimento</span><br/>abre portas</h1>
